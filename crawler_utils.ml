483a2b5b8767c80ff2a2ce0f65b44b20b9b174e1
Vladimir Popescu <vladimir.popescu@elda.org>
2018-02-09 16:47:35 +0100

    Update common file.

--- crawler_utils.ml
+++ crawler_utils.ml
@@ -1,3 +1,28 @@
+(**************************************************************************)
+(* Copyright (C) 2018 Evaluations and Language Resources Distribution     *)
+(* Agency (ELDA) S.A.S (Paris, FRANCE), all rights reserved.              *)
+(* contact http://www.elda.org/ -- mailto:info@elda.org                   *)
+(* author: Vladimir Popescu -- mailto:vladimir@elda.org                   *)
+(*                                                                        *)
+(* This file is part of the ELDA Crawling Management Toolkit (ELDA-CMTK). *)
+(*                                                                        *)
+(* ELDA-CMTK is free software: you can redistribute it and/or modify it   *)
+(* under the terms of the GNU General Public License as published by the  *)
+(* Free Software Foundation, either version 3 of the License, or (at your *)
+(* option) any later version.                                             *)
+(*                                                                        *)
+(* ELDA-CMTK is distributed in the hope that it will be useful, but       *)
+(* WITHOUT ANY WARRANTY; without even the implied warranty of             *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *)
+(* General Public License for more details.                               *)
+(*                                                                        *)
+(* You should have received a copy of the GNU General Public License      *)
+(* along with ELDA-CMTK, in the LICENSE file. If not, see                 *)
+(* <http://www.gnu.org/licenses/>.                                        *)
+(**************************************************************************)
+
+(* Generic common functionalities. *)
+
 open Core
 (** Generate indexes from numeric or character selections.*)
 let indexes_from_selection selection =
